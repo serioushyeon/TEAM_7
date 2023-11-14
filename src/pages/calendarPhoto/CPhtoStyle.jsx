5e64846ec77816491a4815ca7eb85c997c3f63e0
Ohchanju <ohchanju3@naver.com>
2023-11-15 04:03:31 +0900

    feat: 일상 페이지 사진 추가(대표사진 드래그 변경 미구현)

--- src/pages/calendarPhoto/CPhtoStyle.jsx
+++ src/pages/calendarPhoto/CPhtoStyle.jsx
@@ -1,208 +1,259 @@
-import styled from 'styled-components';
-import CameraImoge from '../../assets/images/calendarPhoto/camera.svg';
+import styled from "styled-components";
+import CameraImoge from "../../assets/images/calendarPhoto/camera.svg";
 
 const Container = styled.div`
-position: relative;
-display: flex;
-align-items: center;
-margin: 0 auto;
-max-width: 390px;
-max-height: 844px;
-height: 100%;
-flex-direction: column;
-`
+  position: relative;
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+  max-width: 390px;
+  max-height: 844px;
+  height: 100%;
+  flex-direction: column;
+`;
 
 const Bg = styled.div`
-position: absolute;
-width: 390px;
-height: auto;
-background-color: white;
-`
+  position: relative;
+  width: 390px;
+  height: auto;
+  background-color: white;
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+`;
 
 const DayWeek = styled.div`
-position: absolute;
-top: 3.375rem;
-left: 11.28rem;
-color: #022859;
-font-style: normal;
-font-weight: 400;
-line-height: normal;
-`
+  position: absolute;
+  top: 3.375rem;
+  left: 11.28rem;
+  color: #022859;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
 
 const SmallText = styled.div`
-font-size: 12px;
-font-style: normal;
-font-weight: 400;
-line-height: normal;
-`
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
 
 const DateColor = styled.div`
-position: absolute;
-top: 7.5rem;
-left: 9.9rem;
-color: #000;
-`
+  position: absolute;
+  top: 7.5rem;
+  left: 9.9rem;
+  color: #000;
+`;
 
 const SettingText = styled.div`
-color: #5E5E5E;
-font-style: normal;
-font-weight: 500;
-line-height: normal;
-`
+  color: #5e5e5e;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`;
 
 const SettingPhoto = styled.div`
-position: absolute;
-top: 13.23rem;
-left: 1.43rem;
-`
+  position: absolute;
+  top: 13.23rem;
+  left: 1.43rem;
+`;
 
 const SettingMemo = styled.div`
-position: absolute;
-top: 33.20rem;
-left: 1.43rem;
-`
+  position: absolute;
+  top: 33.2rem;
+  left: 1.43rem;
+`;
 
 // 사진 전체 컨테이너(나중에 배경색 바꿔주세요)
 const PhotoContainer = styled.div`
-display: flex;
-position: absolute;
-flex-wrap: wrap;
-gap: 0.7rem;
-top: 15.125rem;
-left: 01rem;
-width: 21.2rem;
-height: 14.5rem;
-background-color: blue;
-padding: 0.5rem;
-`
+  display: flex;
+  position: absolute;
+  flex-wrap: wrap;
+  gap: 0.7rem;
+  top: 15.125rem;
+  left: 01rem;
+  width: 21.2rem;
+  height: 14.5rem;
+  padding: 0.5rem;
+`;
 
 // 포토박스(이 컴포넌트에 사진 넣으면 알아서 정렬됨(5개)
 const AddPhotoBox = styled.div`
-display: block;
-width: 6.25rem;
-height: 6.25rem;
-border-radius: 4px;
-border: 1px solid #CBCBCB;
-background-color: #FAFAFA;
-`
+  display: flex;
+  width: 6.25rem;
+  height: 6.25rem;
+  border-radius: 4px;
+  border: 1px solid #cbcbcb;
+  /* background-color: #FAFAFA; */
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 10px;
+`;
 
 const CPhotoImage = styled.div`
-width: 42px;
-height: 42px;
-position: absolute;
-left: 2.4rem;
-top: 2.2rem;
-background-image: url(${CameraImoge});
+  width: 42px;
+  height: 42px;
+  display: flex;
 
-`
+  background-image: url(${CameraImoge});
+`;
 
 // 사진 개수 카운트하는 글자 style
 const CPhotoText = styled.div`
-position: absolute;
-left: 5rem;
-top: 5.2rem;
-color: rgba(0, 0, 0, 0.50);
-font-family: Roboto;
-font-size: 14px;
-font-style: normal;
-font-weight: 400;
-`
+  color: rgba(0, 0, 0, 0.5);
+  font-family: Roboto;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  display: flex;
+`;
 
 // 대표사진 표시 박스(추후 위치 지정 해야함)
 const RepresentativePhotoBox = styled.div`
-width: 6.25rem;
-height: 1.625rem;
-border-radius: 0px 0px 4px 4px;
-background: #303030;
-align-items: center;
-text-align: center;
-`
+  width: 6.25rem;
+  height: 1.625rem;
+  border-radius: 0px 0px 4px 4px;
+  background: #303030;
+  align-items: center;
+  text-align: center;
+`;
 
 // 대표 사진 텍스트
 const RepresentativePhotoText = styled.div`
-color: #FFF;
-font-family: Roboto;
-font-size: 12px;
-font-style: normal;
-font-weight: 400;
-`
+  color: white;
+  background-color: #303030;
+  text-align: center;
+  padding: 4px;
+  position: absolute;
+  bottom: 0;
+  width: 92%;
+  border-radius: 0px 0px 4px 4px;
+`;
 
 const MemoBox = styled.textarea`
-position: absolute;
-top: 35rem;
-left: 0.93rem;
-width: 21.25rem;
-height: 5.6rem;
-border-radius: 4px;
-border: 1px solid #CBCBCB;
-outline: none;
-resize: none;
-background-color: #FFF;
-color: #CBCBCB;
-font-family: Noto Sans KR;
-font-size: 12px;
-padding: 0.625rem;
+  position: absolute;
+  top: 35rem;
+  left: 0.93rem;
+  width: 21.25rem;
+  height: 5.6rem;
+  border-radius: 4px;
+  border: 1px solid #cbcbcb;
+  outline: none;
+  resize: none;
+  background-color: #fff;
+  color: #cbcbcb;
+  font-family: Noto Sans KR;
+  font-size: 12px;
+  padding: 0.625rem;
 
-::placeholder {
-    color: #CBCBCB;
-}
-`
+  ::placeholder {
+    color: #cbcbcb;
+  }
+`;
 
 const CalendarButtonStyle = styled.div`
-display: flex;            
-justify-content: center;  
-align-items: center;
-width: 7.5rem;
-height: 2.5rem;
-border-radius: 4px;
-border: 1px solid #CBCBCB;
-color: #BABABA;
-font-size: 12px;
-font-style: normal;
-font-weight: 400;
-line-height: normal;
-` 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 7.5rem;
+  height: 2.5rem;
+  border-radius: 4px;
+  border: 1px solid #cbcbcb;
+  color: #bababa;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
 
 const CancleButton = styled.div`
-position: absolute;
-top: 46.25rem;
-left: 3.3125rem;
-`
+  position: absolute;
+  top: 46.25rem;
+  left: 3.3125rem;
+`;
 
 const SaveButton = styled.div`
-position: absolute;
-top: 46.25rem;
-left: 12.68rem;
-`
+  position: absolute;
+  top: 46.25rem;
+  left: 12.68rem;
+`;
 
 const StyledMaxLength = styled.div`
   position: absolute;
   display: flex;
   top: 40.7rem;
   right: 1.375rem;
-  color: #CBCBCB;
+  color: #cbcbcb;
   font-family: Noto Sans KR;
   font-size: 12px;
 `;
 
+const ImageUploadContainer = styled.div`
+  display: grid;
+  grid-gap: 0;
+  grid-template-columns: 80px 80px auto 1fr;
+  justify-content: center;
+  margin-top: 50px;
+  margin-left: 30px;
+  max-height: calc(100vh - 100px - 120px - 140px);
+  overflow-y: scroll;
+  width: 85%;
+`;
+
+const UploadButton = styled.label`
+  width: 76px;
+  height: 76px;
+  border: 1px solid ${(props) => (props.hasImages ? "black" : "#cbcbcb")};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+  cursor: pointer;
+  border-radius: 4px 0 0 0;
+  flex-direction: column;
+`;
+
+const UploadIcon = styled.img`
+  width: 50%;
+`;
+
+const UploadCount = styled.div`
+  font-size: 12px;
+  margin-top: 10px;
+  color: ${(props) => (props.hasImages ? "black" : "#cbcbcb")};
+`;
+const StyledImage = styled.img`
+  width: 80px;
+  height: 80px;
+  margin: 0;
+  border: ${(props) => (props.isSelected ? "1px solid #F28B50" : "none")};
+`;
+
 export const S = {
-    Container,
-    Bg,
-    DayWeek,
-    SmallText,
-    DateColor,
-    SettingText,
-    SettingPhoto,
-    SettingMemo,
-    PhotoContainer,
-    AddPhotoBox,
-    CPhotoText,
-    CPhotoImage,
-    RepresentativePhotoBox,
-    RepresentativePhotoText,
-    MemoBox,
-    CalendarButtonStyle,
-    CancleButton,
-    SaveButton,
-    StyledMaxLength
-}
+  Container,
+  Bg,
+  DayWeek,
+  SmallText,
+  DateColor,
+  SettingText,
+  SettingPhoto,
+  SettingMemo,
+  PhotoContainer,
+  AddPhotoBox,
+  CPhotoText,
+  CPhotoImage,
+  RepresentativePhotoBox,
+  RepresentativePhotoText,
+  MemoBox,
+  CalendarButtonStyle,
+  CancleButton,
+  SaveButton,
+  StyledMaxLength,
+  ImageUploadContainer,
+  UploadButton,
+  UploadIcon,
+  UploadCount,
+  StyledImage,
+};
--- src/pages/calendarPhoto/CalendarPhoto.jsx
+++ src/pages/calendarPhoto/CalendarPhoto.jsx
@@ -1,64 +1,188 @@
-import React, { useState } from 'react'
-import { S } from './CPhtoStyle';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import React, { useState, useRef } from "react";
+import { S } from "./CPhtoStyle";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 export default function CalendarPhoto() {
-    let navigate = useNavigate();
-     // 취소 버튼 클릭 시 캘린더로 이동
-     function handleLocateCalendar() {
-        navigate('/calendar');
-      }
+  let navigate = useNavigate();
+  // 취소 버튼 클릭 시 캘린더로 이동
+  function handleLocateCalendar() {
+    navigate("/calendar");
+  }
 
-      // 일자 데이터
-    const dateInfo = useSelector((state) => state.date);
+  // 일자 데이터
+  const dateInfo = useSelector((state) => state.date);
 
-    // 메모 데이터
-    const [memo, setMemo] = useState(""); 
-    const maxLength = 100;
+  // 메모 데이터
+  const [memo, setMemo] = useState("");
+  const maxLength = 100;
 
-      // 받은 데이터확인
-    console.log(dateInfo);
+  // 이미지 상태
+  const [images, setImages] = useState([]);
+  const fileInputRef = useRef(null);
 
-    // 메모 실시간 변경
-    const handleMemoChange = (e) => {
-        const value = e.target.value;
+  // 대표 사진 상태
+  const [representativeImageIndex, setRepresentativeImageIndex] =
+    useState(null);
 
-        setMemo(value);
-    };
+  // 받은 데이터확인
+  console.log(dateInfo);
+
+  // 메모 실시간 변경
+  const handleMemoChange = (e) => {
+    const value = e.target.value;
+
+    setMemo(value);
+  };
+
+  // 파일 선택 핸들러 수정
+  const handleFileSelect = (event) => {
+    const files = Array.from(event.target.files).map((file) => ({
+      id: Date.now() + file.name, // 고유 ID 생성
+      file: file,
+    }));
+
+    if (files.length + images.length > 4) {
+      alert("최대 4장의 사진만 업로드 가능합니다.");
+      return;
+    }
+
+    setImages([...images, ...files]);
+  };
+
+  // CPhotoImage 클릭 핸들러
+  const handleImageClick = () => {
+    fileInputRef.current.click();
+  };
+
+  // 이미지 삭제 핸들러
+  const handleRemoveImage = (index) => {
+    setImages(images.filter((_, imgIndex) => imgIndex !== index - 1));
+  };
+
+  // 대표 사진 설정 핸들러
+  const handleSetRepresentative = (index) => {
+    setRepresentativeImageIndex(index - 1);
+  };
+
+  //이미지 드래그 위치 조정
+  const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+  };
+
+  const onDragEnd = (result) => {
+    if (!result.destination) {
+      return;
+    }
+
+    const newImages = reorder(
+      images,
+      result.source.index,
+      result.destination.index
+    );
+
+    setImages(newImages);
+  };
 
   return (
     <S.Container>
-        <S.Bg> 
+      <S.Bg>
         <S.DayWeek>{dateInfo.dayOfWeek}</S.DayWeek>
-        <S.SmallText><S.DateColor>{dateInfo.yearMonthDay}</S.DateColor></S.SmallText>
-        <S.SettingPhoto><S.SettingText>사진 설정</S.SettingText></S.SettingPhoto>
+        <S.SmallText>
+          <S.DateColor>{dateInfo.yearMonthDay}</S.DateColor>
+        </S.SmallText>
+        <S.SettingPhoto>
+          <S.SettingText>사진 설정</S.SettingText>
+        </S.SettingPhoto>
         <S.PhotoContainer>
-        <S.AddPhotoBox>
-            <S.CPhotoImage />
-            <S.CPhotoText>4/4</S.CPhotoText>
-            </S.AddPhotoBox>
-            <S.AddPhotoBox />
-            <S.AddPhotoBox />
-            <S.AddPhotoBox />
-            <S.AddPhotoBox />
-            </S.PhotoContainer>
-        <S.SettingMemo><S.SettingText>메모</S.SettingText></S.SettingMemo>
-        <S.MemoBox 
-        placeholder="아직 작성된 일상 메모가 없습니다."
-        name="memo"
-        value={memo}
-        onChange={handleMemoChange}
-        maxLength={100} />
-         <S.StyledMaxLength>{`${memo.length}/${maxLength}`}</S.StyledMaxLength>
+          {Array.from({ length: 5 }).map((_, index) => {
+            const image = images[index - 1];
+            return (
+              <S.AddPhotoBox
+                key={index}
+                id={image ? image.id : `add-photo-box-${index}`} // 고유 ID 사용
+                onClick={index === 0 ? handleImageClick : null}
+                style={{ position: "relative" }}
+              >
+                {/* X 아이콘 추가 및 클릭 핸들러 연결 */}
+                {index !== 0 && image && (
+                  <div
+                    className="delete-icon"
+                    onClick={() => handleRemoveImage(index)}
+                    style={{
+                      position: "absolute",
+                      top: "5px",
+                      right: "5px",
+                      cursor: "pointer",
+                      color: "white",
+                    }}
+                  >
+                    X
+                  </div>
+                )}
+                {index === 0 ? (
+                  <>
+                    <S.CPhotoImage />
+                    <S.CPhotoText>{images.length}/4</S.CPhotoText>
+                  </>
+                ) : (
+                  <>
+                    {images[(image, index - 1)] && (
+                      <img
+                        src={URL.createObjectURL(image.file)}
+                        alt={`Uploaded ${index - 1}`}
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                          objectFit: "cover",
+                        }}
+                      />
+                    )}
+                    {index === 1 && (
+                      <S.RepresentativePhotoText>
+                        대표 사진
+                      </S.RepresentativePhotoText>
+                    )}
+                  </>
+                )}
+              </S.AddPhotoBox>
+            );
+          })}
+          <input
+            type="file"
+            ref={fileInputRef}
+            style={{ display: "none" }}
+            multiple
+            onChange={handleFileSelect}
+          />
+        </S.PhotoContainer>
+
+        <S.SettingMemo>
+          <S.SettingText>메모</S.SettingText>
+        </S.SettingMemo>
+        <S.MemoBox
+          placeholder="아직 작성된 일상 메모가 없습니다."
+          name="memo"
+          value={memo}
+          onChange={handleMemoChange}
+          maxLength={100}
+        />
+        <S.StyledMaxLength>{`${memo.length}/${maxLength}`}</S.StyledMaxLength>
         <S.CancleButton>
-            <S.CalendarButtonStyle onClick={handleLocateCalendar}>취소</S.CalendarButtonStyle>
-            </S.CancleButton>
+          <S.CalendarButtonStyle onClick={handleLocateCalendar}>
+            취소
+          </S.CalendarButtonStyle>
+        </S.CancleButton>
         <S.SaveButton>
-            <S.CalendarButtonStyle>저장</S.CalendarButtonStyle>
-            </S.SaveButton>
-        </S.Bg>
+          <S.CalendarButtonStyle>저장</S.CalendarButtonStyle>
+        </S.SaveButton>
+      </S.Bg>
     </S.Container>
-  )
+  );
 }
