a81b7391bd5c46a6a4ae19eaa28cf05d5199b06e
Eunji <bian87@dgu.ac.kr>
2023-11-16 00:30:35 +0900

    add : socket env 파일 추가

--- src/api/ApiClient.jsx
+++ src/api/ApiClient.jsx
@@ -3,3 +3,7 @@ import axios from "axios";
 export const apiClient = axios.create({
   baseURL: import.meta.env.VITE_APP_SERVER_HOST,
 });
+
+export const apiSocket = axios.create({
+  baseURL: import.meta.env.VITE_APP_SERVER_WEBSOCKET,
+});
