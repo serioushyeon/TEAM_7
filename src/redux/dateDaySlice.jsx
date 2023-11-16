f3bbfabad285ebca3eb9d72024044fe581c118f5
Eunji <bian87@dgu.ac.kr>
2023-11-17 07:07:14 +0900

    comment : 주석 삭제

--- src/redux/dateDaySlice.jsx
+++ src/redux/dateDaySlice.jsx
@@ -18,63 +18,17 @@ export const dateDaySlice = createSlice({
         updateDay: (state, action) => {
             const { day } = action.payload;
             state.dateDay.day = day;
-/* 유효성 검사
-    let newYear = parseInt(state.dateDay.year, 10); // 문자열을 숫자로 변환
-    let newMonth = parseInt(state.dateDay.month, 10); // 문자열을 숫자로 변환
-    let newDay = day;
-
-
-  const lastDayOfMonth = new Date(newYear, newMonth + 1, 0).getDate();
-
-  if (newDay > lastDayOfMonth) {
-    newDay = 1;
-    newMonth += 1;
-    if (newMonth > 11) {
-      newMonth = 0;
-      newYear += 1;
-    }
-  } else if (newDay < 1) {
-    newMonth -= 1;
-    if (newMonth < 0) {
-      newMonth = 11;
-      newYear -= 1;
-    }
-    newDay = new Date(newYear, newMonth + 1, 0).getDate();
-  }
-
-  state.dateDay = { year: newYear, month: newMonth, day: newDay };
-  */
             },
             
         // 월 변경
     updateMonth: (state, action) => {
         const { month } = action.payload;
             state.dateDay.month = month; 
-  
-            /*
-        let newMonth = month;
-        let newYear = year;
-  
-        if (newMonth > 11) {
-          newMonth = 0;
-          newYear += 1;
-        } else if (newMonth < 0) {
-          newMonth = 11;
-          newYear -= 1;
-        }
-  
-        state.dateDay = { ...state.dateDay, year: newYear, month: newMonth };
-        */
       },
       // 년 변경
       updateYear: (state, action) => {
         const { year } = action.payload;
             state.dateDay.year = year;
-            /*
-        let newYear = year;
-
-        state.dateDay = { ...state.dateDay, year: newYear };
-        */
       },
     }
 
