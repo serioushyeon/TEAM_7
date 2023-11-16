edaacead14bf2b2d24eb84a4f8bd4be60af0cf89
serioushyeon <zz64446949@gmail.com>
2023-11-17 04:59:02 +0900

    design: 캘린더 css 수정

--- src/redux/eventListSlice.jsx
+++ src/redux/eventListSlice.jsx
@@ -4,15 +4,64 @@ import {format} from 'date-fns';
 export const eventListSlice = createSlice({
     name: "eventList",
     initialState: {
-		profileImgUrlList:["https://via.placeholder.com/150"],
+		profileImgUrlList:["https://via.placeholder.com/150","https://via.placeholder.com/150","https://via.placeholder.com/150","https://via.placeholder.com/150","https://via.placeholder.com/150","https://via.placeholder.com/150","https://via.placeholder.com/150"],
 		isRoomMaker : true,
 		eventName : "이벤트 명",   
 		startDate : `${format(new Date(), "yyyy-MM-dd")}`,   //Date의 형식은 2023-10-09, 2023-09-07 이런형식!!
 		endDate : `${format(new Date(), "yyyy-MM-dd")}`,     //Date의 형식은 2023-10-09, 2023-09-07 이런형식!!
-		loginUserId : "",
-		userCount : 1,
+		loginUserId : "123",
+		userCount : 7,
 		userInfo:
-		[]
+		[{
+			userId: "123",
+			nickname: "방장",
+			imageList: ["https://via.placeholder.com/150","https://via.placeholder.com/150","https://via.placeholder.com/150"],
+			checkStatus: true,
+			imageCount: 3
+		},
+		{
+			userId: "456",
+			nickname: "user2",
+			imageList: ["https://via.placeholder.com/150","https://via.placeholder.com/150"],
+			checkStatus: true,
+			imageCount: 2
+		},
+		{
+			userId: "789",
+			nickname: "user3",
+			imageList: ["https://via.placeholder.com/150","https://via.placeholder.com/150","https://via.placeholder.com/150"],
+			checkStatus: true,
+			imageCount: 3
+		},
+		{
+			userId: "000",
+			nickname: "user4",
+			imageList: ["https://via.placeholder.com/150","https://via.placeholder.com/150","https://via.placeholder.com/150"],
+			checkStatus: true,
+			imageCount: 3
+		},
+		{
+			userId: "111",
+			nickname: "user5",
+			imageList: ["https://via.placeholder.com/150","https://via.placeholder.com/150","https://via.placeholder.com/150"],
+			checkStatus: true,
+			imageCount: 3
+		},
+		{
+			userId: "222",
+			nickname: "user7",
+			imageList: ["https://via.placeholder.com/150","https://via.placeholder.com/150","https://via.placeholder.com/150"],
+			checkStatus: true,
+			imageCount: 3
+		},
+		{
+			userId: "333",
+			nickname: "user8",
+			imageList: ["https://via.placeholder.com/150","https://via.placeholder.com/150","https://via.placeholder.com/150"],
+			checkStatus: true,
+			imageCount: 3
+		},
+	]
 	},
     reducers: {
         setEventList: (state, action) => {
