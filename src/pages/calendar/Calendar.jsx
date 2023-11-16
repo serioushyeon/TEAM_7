7f47d2df9bcac56cc971cdc4d701e68d3578352f
Eunji <bian87@dgu.ac.kr>
2023-11-17 08:58:04 +0900

    design 일별 이미지 사이즈 비율 조정

--- src/pages/calendar/Calendar.jsx
+++ src/pages/calendar/Calendar.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { apiClient } from '../../api/ApiClient';
+import { useCookies } from "react-cookie";
 
 import { selectDate } from '../../redux/dateSlice';
 import { setActiveStartDate } from '../../redux/CalendarUI';
@@ -43,11 +44,12 @@ const imageData = {
     // 리듀서들
     const activeStartDateString = useSelector((state) => state.calendarUI.activeStartDate);
     const dateRange = useSelector(state => state.dateRange.dateRange);
-  
-
     // 설정 해야함
     console.log('startDate: ', dateRange.startDate, 'endDate: ', dateRange.endDate);
     console.log('year:', dateRange.year, 'month: ', dateRange.month);
+
+    const [accessCookie] = useCookies(["accessCookie"]);
+    const [refreshCookie] = useCookies(["refreshCookie"]);
     
     const [calendarInfo, setCalendarInfo] = useState({
       thumbnailInfoList:
