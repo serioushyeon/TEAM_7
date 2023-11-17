9c1f48888959014a98afa200a100d07819a32246
serioushyeon <zz64446949@gmail.com>
2023-11-18 05:38:46 +0900

    fix

--- src/pages/EventDisplay/EventDisplay.jsx
+++ src/pages/EventDisplay/EventDisplay.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
 import EventHeader from "../../components/EventHeader/EventHeader";
 import EventParticipants from "../../components/EventParticipants/EventParticipants";
 import EventUploadList from "../../components/EventUploadList/EventUploadList";
