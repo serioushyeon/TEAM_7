79daa5cf8265173ebc1393e08183a839b0dff97e
Eunji <bian87@dgu.ac.kr>
2023-11-17 16:35:06 +0900

    fix : 캘린더 각종 date 날짜 수정

--- src/redux/dateRangeSlice.jsx
+++ src/redux/dateRangeSlice.jsx
@@ -15,27 +15,33 @@ export const dateRangeSlice = createSlice({
     reducers: {
         // 날짜의 전체 범위 설정
         updateDateRange: (state, action) => {
-            const { year, month } = action.payload;
-            const startOfMonth = new Date(year, month - 1, 1);
-            const endOfMonth = new Date(year, month, 0);
-      
+            let { year, month } = action.payload;
+            month += 1;
+
+            const startOfMonth = new Date(year, month - 1, 1); // 월에서 1을 빼서 Date 객체에 전달
+            const endOfMonth = new Date(year, month, 0); // 다음 달의 0번째 날짜는 이번 달의 마지막 날짜임
+        
             const startDayOfWeek = startOfMonth.getDay() === 0 ? 6 : startOfMonth.getDay() - 1;
-            const endDayOfWeek = endOfMonth.getDay() === 0 ? 0 : 7 - endOfMonth.getDay();
-      
+            const endDayOfWeek = endOfMonth.getDay() === 0 ? 0 : 6 - endOfMonth.getDay();
+        
             const startOfCalendar = new Date(startOfMonth);
             startOfCalendar.setDate(startOfMonth.getDate() - startDayOfWeek);
-      
+        
             const endOfCalendar = new Date(endOfMonth);
             endOfCalendar.setDate(endOfMonth.getDate() + endDayOfWeek);
-      
-            const formatDate = (date) => date.toISOString().split('T')[0];
-      
+        
+            const formatDate = (date) => {
+              const adjustedMonth = date.getMonth() + 1; // 실제 월 표시를 위해 1 증가
+              const day = date.getDate();
+              return `${date.getFullYear()}-${adjustedMonth.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+            };
+        
             state.dateRange.startDate = formatDate(startOfCalendar);
             state.dateRange.endDate = formatDate(endOfCalendar);
             state.dateRange.year = year;
-            state.dateRange.month = month;
+            state.dateRange.month = month; // 실제 월 저장
           }
-    }
+        }
 
 });
 
