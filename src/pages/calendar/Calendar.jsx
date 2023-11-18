fb22071920e4c74b81292f87204387bbef50baa6
Eunji <bian87@dgu.ac.kr>
2023-11-18 19:27:04 +0900

    fix : calendar get method

--- src/pages/calendar/Calendar.jsx
+++ src/pages/calendar/Calendar.jsx
@@ -58,9 +58,7 @@ export default function MyCalendar() {
         const response = await axios.get("/api/v1/user/calender", {
           params: {
             startDate: moment(activeStartDate).format("YYYY-MM-DD"),
-            endDate: moment(activeStartDate)
-              .endOf("month")
-              .format("YYYY-MM-DD"),
+            endDate: moment(activeStartDate),
             year,
             month,
           },
