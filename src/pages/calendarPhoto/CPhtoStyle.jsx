3465744193551d915a14620f8d48a4bfc7ac7383
Eunji <bian87@dgu.ac.kr>
2023-11-28 16:38:11 +0900

    refactor : 사진 갤러리 로직 수정

--- src/pages/calendarPhoto/CPhtoStyle.jsx
+++ src/pages/calendarPhoto/CPhtoStyle.jsx
@@ -63,6 +63,19 @@ const PhotoContainer = styled.div`
 `;
 
 // 포토박스(이 컴포넌트에 사진 넣으면 알아서 정렬됨(5개)
+const PhotoBox = styled.div`
+  display: flex;
+  position: relative;
+  width: 30%;
+  height: 100px;
+  border-radius: 4px;
+  border: 1px solid #cbcbcb;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+// 포토박스(이 컴포넌트에 사진 넣으면 알아서 정렬됨(5개)
 const AddPhotoBox = styled.div`
   display: flex;
   width: 30%;
@@ -72,19 +85,50 @@ const AddPhotoBox = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  gap: 10px;
 `;
 
-const CPhotoImage = styled.div`
+const PhotoImage = styled.img`
+  height: 100%;
+  width: 100%;
+  border-radius: 4px;
+  object-fit: cover;
+  background-size: contain;
+`;
+
+const AddPhoto = styled.input`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  border: 0;
+`;
+
+const AddPhotoLabel = styled.label`
+  position: absolute;
+  display: flex;
+  padding: 31px;
+  text-align: center;
+  align-items: center; // 수직 중앙 정렬
+  justify-content: center; // 수평 중앙 정렬
+  top: 25.3%;
+  left: 5%;
+  cursor: pointer;
+`;
+
+const AddPhotoImage = styled.div`
   width: 42px;
   height: 42px;
   display: flex;
-
   background-image: url(${CameraImoge});
 `;
 
 // 사진 개수 카운트하는 글자 style
-const CPhotoText = styled.div`
+const CountText = styled.div`
+  margin-top: 70px;
+  margin-left: 70px;
   color: rgba(0, 0, 0, 0.5);
   font-family: Roboto;
   font-size: 14px;
@@ -93,13 +137,25 @@ const CPhotoText = styled.div`
   display: flex;
 `;
 
+const DeleteText = styled.button`
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  font-size: 16px;
+  color: white;
+  outline: none;
+  cursor: pointer;
+  border: none;
+  background: none;
+`;
+
 // 대표 사진 텍스트
 const RepresentativePhotoText = styled.div`
+  position: absolute;
   color: white;
   background-color: #303030;
   text-align: center;
   padding: 4px;
-  position: absolute;
   bottom: 0;
   width: 92%;
   height: 20px;
@@ -144,7 +200,7 @@ const UploadChange = styled.div`
   margin-bottom: 30px;
 `;
 
-const UploadChangeItem = styled.div`
+const UploadChangeItem = styled.button`
   width: 120px;
   height: 40px;
   border: 1px solid ${(props) => (props.isSelected ? "#F28B50" : "#cbcbcb")};
@@ -165,9 +221,14 @@ export const S = {
   SettingPhoto,
   SettingMemo,
   PhotoContainer,
+  PhotoImage,
   AddPhotoBox,
-  CPhotoText,
-  CPhotoImage,
+  PhotoBox,
+  AddPhoto,
+  AddPhotoImage,
+  AddPhotoLabel,
+  CountText,
+  DeleteText,
   RepresentativePhotoText,
   MemoBox,
   // CalendarButtonStyle,
--- src/pages/calendarPhoto/CalendarPhoto.jsx
+++ src/pages/calendarPhoto/CalendarPhoto.jsx
@@ -2,41 +2,31 @@ import React, { useState, useRef, useEffect } from "react";
 import { S } from "./CPhtoStyle";
 import axios from "axios";
 import { useNavigate, useLocation } from "react-router-dom";
-import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
-import { setCalendarData } from "../../redux/CalendarPhotoBoard";
 import PhotoOption from "../../components/calendar/PhotoOption";
 import { useParams } from "react-router-dom";
 import { useCookies } from "react-cookie";
 export default function CalendarPhoto() {
   let navigate = useNavigate();
-  const dispatch = useDispatch();
   const location = useLocation();
   const { date } = useParams();
+
   // 요일 데이터
   const { dayOfWeek } = location.state || {};
   const dateRedux = useSelector((state) => state.dateDay); // redux의 dateDay 상태
 
-  // Redux 스토어에서 데이터 가져오기
-  const calendarPhotoData = useSelector((state) => state.CalendarPhotoBoard);
+  const [fileStatus, setFileStatus] = useState([]);
+  const [memo, setMemo] = useState("");
+  const [images, setImages] = useState("");
+  const [draggedImage, setDraggedImage] = useState(null);
 
-  // 컴포넌트 상태 초기화
-  const [memo, setMemo] = useState(calendarPhotoData?.memo || "");
-  const [images, setImages] = useState(calendarPhotoData?.images || []);
-  // const [memo, setMemo] = useState("");
+  console.log("file : ", fileStatus);
+  console.log("memo : ", memo);
+
   const maxLength = 100;
 
-  const [getAccessCookie, setAccessCookie, removeCookie] = useCookies([
-    "access_cookie",
-  ]);
+  const [getAccessCookie, removeCookie] = useCookies(["access_cookie"]);
 
-  console.log("images : ", images);
-  // 이미지 상태
-  // const [images, setImages] = useState([]);
-  const fileInputRef = useRef(null);
-  // 대표 사진 상태
-  const [representativeImageIndex, setRepresentativeImageIndex] =
-    useState(null);
   // 로그아웃 처리 함수
   const handleLogout = () => {
     // 쿠키 삭제
@@ -48,88 +38,93 @@ export default function CalendarPhoto() {
     // 로그인 페이지로 리다이렉트
     navigate("/");
   };
-  useEffect(() => {
-    const fetchDayData = async () => {
-      try {
-        const response = await axios.get(`/api/v1/user/${date}`);
-        const fetchedData = response.data;
-        // 가져온 데이터로 상태 업데이트
-        setMemo(fetchedData.memo);
-        setImages(
-          fetchedData.dayImageList.map((imageUrl, index) => ({
-            id: index,
-            file: new File([], imageUrl), // File 객체 생성 (또는 다른 방식 사용)
-          }))
-        );
-      } catch (error) {
-        if (error.response && error.response.status === 404) {
-          if (error.response.data.code === "DAY_NOT_FOUND") {
-            // 'DAY_NOT_FOUND' 에러 처리
-            alert("정상적이지 않은 경로입니다. 캘린더 페이지로 이동합니다.");
-            navigate("/calendar");
-          } else if (error.response.data.code === "USER_NOT_FOUND") {
-            // 'USER_NOT_FOUND' 에러 처리
-            alert(
-              "로그인한 사용자가 존재하지 않습니다. 로그인 페이지로 이동합니다."
-            );
-            handleLogout();
-          } else {
-            console.error("Error fetching day data", error);
-          }
+
+  // 파일도 같이 받아와야할듯
+  const getDayData = async () => {
+    console.log("date : ", date);
+    try {
+      const response = await axios.get(`/api/v1/user/${date}`);
+
+      // 데이터는 useState에 세팅한다.
+      console.log("받은 데이터 : ", response.data);
+
+      // 나중에 받은 데이터 파일로 변경해야함.
+      setImages(response.data.dayImageList);
+      setMemo(response.data.memo);
+    } catch (error) {
+      if (error.response && error.response.status === 404) {
+        if (error.response.data.code === "DAY_NOT_FOUND") {
+          // 'DAY_NOT_FOUND' 에러 처리
+          alert("정상적이지 않은 경로입니다. 캘린더 페이지로 이동합니다.");
+          navigate("/calendar");
+        } else if (error.response.data.code === "USER_NOT_FOUND") {
+          // 'USER_NOT_FOUND' 에러 처리
+          alert(
+            "로그인한 사용자가 존재하지 않습니다. 로그인 페이지로 이동합니다."
+          );
+          handleLogout();
         } else {
           console.error("Error fetching day data", error);
         }
+      } else {
+        console.error("Error fetching day data", error);
       }
-    };
-    fetchDayData();
-  }, [date]);
+    }
+  };
+
+  // 초기 렌더링 시 데이터를 불러온다.
+  useEffect(() => {
+    getDayData();
+  }, []);
+
   // 메모 실시간 변경
   const handleMemoChange = (e) => {
-    const value = e.target.value;
-    setMemo(value);
+    setMemo(e.value.target);
+    console.log("memo : ", memo);
   };
-  // 파일 선택 핸들러 수정
+
+  // 파일 선택 핸들러
   const handleFileSelect = (event) => {
     const files = Array.from(event.target.files).map((file) => ({
       id: Date.now() + file.name, // 고유 ID 생성
       file: file,
     }));
-    if (files?.length + images?.length > 4) {
+
+    // 파일 개수 제한 확인
+    if (fileStatus?.length > 4 || files.length > 4) {
       alert("최대 4장의 사진만 업로드 가능합니다.");
       return;
     }
-    setImages([...images, ...files]);
+
+    // fileStatus 업데이트
+    setFileStatus((prevStatus) => [...prevStatus, ...files]);
   };
-  // CPhotoImage 클릭 핸들러
-  const handleImageClick = () => {
-    fileInputRef.current.click();
+
+  const handleDragStart = (index) => {
+    setDraggedImage(index);
   };
-  // 이미지 삭제 핸들러
-  const handleRemoveImage = (index) => {
-    setImages(images.filter((_, imgIndex) => imgIndex !== index - 1));
+
+  const handleDragOver = (e) => {
+    e.preventDefault(); // 드래그 앤 드롭 동작을 가능하게 함
   };
-  // 대표 사진 설정 핸들러
-  const handleSetRepresentative = (index) => {
-    setRepresentativeImageIndex(index - 1);
+
+  const handleDrop = (index) => {
+    const updatedFiles = [...fileStatus];
+    const draggedFile = updatedFiles[draggedImage];
+    updatedFiles.splice(draggedImage, 1);
+    updatedFiles.splice(index, 0, draggedFile);
+
+    setFileStatus(updatedFiles);
+    setDraggedImage(null);
   };
-  //이미지 드래그 위치 조정
-  const reorder = (list, startIndex, endIndex) => {
-    const result = Array.from(list);
-    const [removed] = result.splice(startIndex, 1);
-    result.splice(endIndex, 0, removed);
-    return result;
-  };
-  const onDragEnd = (result) => {
-    if (!result.destination) {
-      return;
-    }
-    const newImages = reorder(
-      images,
-      result.source.index,
-      result.destination.index
+
+  // 이미지 삭제 함수
+  const handleDelete = (indexToDelete) => {
+    setFileStatus((prevStatus) =>
+      prevStatus.filter((_, index) => index !== indexToDelete)
     );
-    setImages(newImages);
   };
+
   const postCalendarData = async () => {
     try {
       // FormData 객체 생성
@@ -137,50 +132,42 @@ export default function CalendarPhoto() {
       // 메모 추가
       formData.append("memo", memo);
       // 이미지 추가
-      if (images?.length > 0) {
+      if (fileStatus?.length > 0) {
         // 첫 번째 이미지는 'thumbnail'로 추가
-        formData.append("thumbnail", images[0].file);
+        formData.append("thumbnail", fileStatus[0].file);
       }
+
       // 나머지 이미지들은 'photo1', 'photo2', 'photo3'로 추가
-      images.slice(1).forEach((image, index) => {
-        formData.append(`photo${index + 1}`, image.file);
+      fileStatus.slice(1).forEach((file, index) => {
+        formData.append(`photo${index + 1}`, file.file);
       });
-      // FormData 객체 내용 확인을 위한 콘솔 로그
-      // for (let [key, value] of formData.entries()) {
-      //   console.log(`${key}: `, value);
-      // }
+
+      console.log("formData : ", formData);
+
       // API 요청
-      const response = await axios.post(
-        `/api/v1/user/${dateRedux.date}`,
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-            Authorization: `Bearer ${getAccessCookie}`,
-          },
-        }
-      );
+      const response = await axios.post(`/api/v1/user/${date}`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${getAccessCookie}`,
+        },
+      });
       console.log("성공 :", response.data);
     } catch (error) {
       console.error("에러 : ", error);
-      console.log(
-        "Data attempted to post:",
-        images.map((image) => image.file)
-      );
     }
   };
+
   // 취소 버튼 클릭 시 캘린더로 이동
-  function handleLocateCalendar() {
+  function handleCancle() {
     navigate("/calendar");
   }
 
   const handleSave = () => {
     postCalendarData();
-    dispatch(setCalendarData({ memo, images }));
     navigate("/calendar");
   };
 
-  const [previousDate, setPreviousDate] = useState(date); // 이전 날짜 상태
+  /*const [previousDate, setPreviousDate] = useState(date); // 이전 날짜 상태
   useEffect(() => {
     // 날짜가 변경되었을 때 초기화 로직
     if (date !== previousDate) {
@@ -189,13 +176,8 @@ export default function CalendarPhoto() {
       setPreviousDate(date); // 이전 날짜 업데이트
     }
   }, [date, previousDate]);
-  useEffect(() => {
-    // 컴포넌트가 언마운트될 때 실행되는 로직
-    return () => {
-      setImages([]); // 이미지 초기화
-      setMemo(""); // 메모 초기화
-    };
-  }, []);
+  */
+
   return (
     <S.Container>
       <PhotoOption date={date} />
@@ -208,71 +190,47 @@ export default function CalendarPhoto() {
       </S.SettingPhoto>
       <S.PhotoWrapper>
         <S.PhotoContainer>
-          {Array.from({ length: 5 }).map((_, index) => {
-            const image = index > 0 ? images[index - 1] : null;
-            // 첫 번째 줄과 두 번째 줄 구분
-            const isSecondRow = index >= 3;
-            return (
-              <S.AddPhotoBox
-                key={index}
-                id={image ? image.id : `add-photo-box-${index}`} // 고유 ID 사용
-                onClick={index === 0 ? handleImageClick : null}
-                style={{
-                  position: "relative",
-                  marginBottom: isSecondRow ? "20px" : "0", // 두 번째 줄 사진 상자는 아래에 간격 추가
-                }}
-              >
-                {/* X 아이콘 추가 및 클릭 핸들러 연결 */}
-                {index !== 0 && image && (
-                  <div
-                    className="delete-icon"
-                    onClick={() => handleRemoveImage(index)}
-                    style={{
-                      position: "absolute",
-                      top: "5px",
-                      right: "5px",
-                      cursor: "pointer",
-                      color: "white",
-                    }}
-                  >
-                    X
-                  </div>
-                )}
-                {index === 0 ? (
+          <S.AddPhotoBox>
+            <S.AddPhotoLabel htmlFor="fileStatus">
+              <S.AddPhotoImage />
+            </S.AddPhotoLabel>
+            <S.CountText>{fileStatus.length}/4</S.CountText>
+            <S.AddPhoto
+              type="file"
+              id="fileStatus"
+              name="fileStatus"
+              multiple
+              onChange={handleFileSelect}
+            />
+          </S.AddPhotoBox>
+          {
+            // 길이가 4인 배열을 생성하고, 각 요소에 대해 반복
+            new Array(4).fill().map((_, index) => (
+              <S.PhotoBox key={index}>
+                {fileStatus && fileStatus[index] ? (
                   <>
-                    <S.CPhotoImage />
-                    <S.CPhotoText>{images?.length}/4</S.CPhotoText>
+                    <S.PhotoImage
+                      src={URL.createObjectURL(fileStatus[index].file)}
+                      draggable
+                      onDragStart={() => handleDragStart(index)}
+                      onDragOver={handleDragOver}
+                      onDrop={() => handleDrop(index)}
+                    />
+                    <S.DeleteText onClick={() => handleDelete(index)}>
+                      x
+                    </S.DeleteText>
                   </>
                 ) : (
-                  <>
-                    {images[(image, index - 1)] && (
-                      <img
-                        src={URL.createObjectURL(image.file)}
-                        alt={`Uploaded ${index - 1}`}
-                        style={{
-                          width: "100%",
-                          height: "100%",
-                          objectFit: "cover",
-                        }}
-                      />
-                    )}
-                    {index === 1 && (
-                      <S.RepresentativePhotoText>
-                        대표 사진
-                      </S.RepresentativePhotoText>
-                    )}
-                  </>
+                  <></>
                 )}
-              </S.AddPhotoBox>
-            );
-          })}
-          <input
-            type="file"
-            ref={fileInputRef}
-            style={{ display: "none" }}
-            multiple
-            onChange={handleFileSelect}
-          />
+                {index === 0 && (
+                  <S.RepresentativePhotoText>
+                    대표사진
+                  </S.RepresentativePhotoText>
+                )}
+              </S.PhotoBox>
+            ))
+          }
         </S.PhotoContainer>
       </S.PhotoWrapper>
       <S.SettingMemo>
@@ -287,10 +245,10 @@ export default function CalendarPhoto() {
         <S.StyledMaxLength>{`${memo?.length}/${maxLength}`}</S.StyledMaxLength>
       </S.SettingMemo>
       <S.UploadChange>
-        <S.UploadChangeItem onClick={handleLocateCalendar}>
-          취소
+        <S.UploadChangeItem onClick={handleCancle}>취소</S.UploadChangeItem>
+        <S.UploadChangeItem type="submit" onClick={handleSave}>
+          저장
         </S.UploadChangeItem>
-        <S.UploadChangeItem onClick={handleSave}>저장</S.UploadChangeItem>
       </S.UploadChange>
     </S.Container>
   );
